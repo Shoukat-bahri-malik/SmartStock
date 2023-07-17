af4b4137ff5a5e1bef002b6883fdbf5d90c71067
Shoukat Malik <shoukatmalik.dev@gmail.com>
2023-07-18 00:15:20 +0500

    Added Remaining Entities

---
+++ SmartStock.Models/NumberSequence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartStock.Models
+{
+    public class NumberSequence
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Module { get; set; } = string.Empty;
+        [Required]
+        public string Prefix { get; set; } = string.Empty;
+        public int LastNumber { get; set; }
+    }
+}
---
+++ SmartStock.Models/ReceivedNote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartStock.Models
+{
+    public class ReceivedNote
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PurchaseOrderId { get; set; }
+        public DateTimeOffset GRNDate { get; set; }
+        public string VenderNumber { get; set; }
+        public string VendorInvoiceNumber { get; set; }
+        public int WarehouseId { get; set; }
+        public bool IsFullRecieve { get; set; }
+    }
+}
